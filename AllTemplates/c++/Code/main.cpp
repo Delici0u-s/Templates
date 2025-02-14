8a283b5f76c4ed1b20adad2d0e53c4962f626271
Delici0u-s <maxi.bravo111@gmail.com>
2025-02-14 21:58:57 +0100

    updated c++ template

--- AllTemplates/c++/Code/main.cpp
+++ AllTemplates/c++/Code/main.cpp
@@ -1,6 +1,11 @@
 #include "src/tmp.hpp"
+#include <filesystem>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
+  std::cout << "I am being run from: " << std::filesystem::current_path() << "\n";
+
+  // this isnt
   print("Argv: ");
   for (int i{1}; i < argc; ++i) {
     print(argv[i]);
