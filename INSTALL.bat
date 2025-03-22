88205a66fa07b730e988319070b2a74370c2927c
Delici0u-s <maxi.bravo111@gmail.com>
2025-03-22 12:36:54 +0100

    Fixed template installing

--- INSTALL.bat
+++ INSTALL.bat
@@ -7,7 +7,7 @@ powershell -noprofile -executionpolicy bypass -file amca_impl/setpath.ps1
 
 mkdir "%LOCALAPPDATA%\amca\templates" 2>nul
 
-for %%F in (./amca_impl/templates/*.json) do (
-    echo Extracting "./amca_impl/templates/%%F" to "%LOCALAPPDATA%/amca/templates/"
-    python amca_impl/dearchiver.py "./amca_impl/templates/%%~F" "%LOCALAPPDATA%/amca/templates/"
+for /D %%F in (./amca_impl/blueprints/*) do (
+    echo installing template: %%F
+    cp ./amca_impl/blueprints/%%F "%LOCALAPPDATA%\amca\templates" -r
 )
